4395866a78bca5a7318b2528ce5d56b17407e0b4
Chisaneme Aloni <chisanemealoni@gmail.com>
2023-07-01 01:24:03 +0100

    small changes & update

--- learn-javascript-main/3. Build a Blackjack game/51. Create your first object/index.js
+++ learn-javascript-main/3. Build a Blackjack game/51. Create your first object/index.js
@@ -2,4 +2,11 @@
 // It should contain at least one boolean, one string, one number, and one array
 // Log out at least two of the keys using the dot notation
 
+let castle = {
+  name: "Scottish Highland Castle",
+  isMassive: true,
+  build: 3,
+  gate: ["west", "east", "south", "north"],
+};
 
+console.log(castle.isMassive);
