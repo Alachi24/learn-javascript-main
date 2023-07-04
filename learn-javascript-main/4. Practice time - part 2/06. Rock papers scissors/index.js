0585b32005c27f41f9923230413aab0c513d4a31
Chisaneme Aloni <chisanemealoni@gmail.com>
2023-07-04 22:45:53 +0100

    modification & update

--- learn-javascript-main/4. Practice time - part 2/06. Rock papers scissors/index.js
+++ learn-javascript-main/4. Practice time - part 2/06. Rock papers scissors/index.js
@@ -1,5 +1,10 @@
-let hands = ["rock", "paper", "scissor"]
+let hands = ["rock", "paper", "scissor"];
 
 // Create a function that returns a random item from the array
 
+function item() {
+  let randomItem = Math.floor(Math.random() * 3);
+  return hands[randomItem];
+}
 
+console.log(item());
