b68fd646232bf2c8e7bb42375ba27624f567e51f
Chisaneme Aloni <chisanemealoni@gmail.com>
2023-07-19 21:42:37 +0100

    update on the task given

--- learn-javascript-main/5. Build a Chrome Extension/27. Template strings on multiple lines/index.js
+++ learn-javascript-main/5. Build a Chrome Extension/27. Template strings on multiple lines/index.js
@@ -1,9 +1,16 @@
 // template strings/literals
 
-const recipient = "James"
-const sender = "Per Harald Borgen"
+const recipient = "James";
+const sender = "Chisaneme Aloni";
 
 // Break the email string into multiple lines
-const email = `Hey ${recipient}! How is it going? Cheers ${sender}`
+const email = `Hey ${recipient}!
+ How is it going?
+ Cheers ${sender}`;
 
-console.log(email)
+console.log(email);
+
+// Adding a continuation line(\) allows it to still be in one line when consoled
+// `Hey ${recipient}!\
+//  How is it going?\
+//  Cheers ${sender}`;
